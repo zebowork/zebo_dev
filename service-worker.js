0f923eabeecf2955eab3a62f54007a3700cf9f47
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-06-01 17:34:13 +0100

    deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c8cec5d34ff3732f28ffec0cd5f7750.js"
+  "/precache-manifest.f0387601c586c126bb781dd3c3d16439.js"
 );
 
 self.addEventListener('message', (event) => {
